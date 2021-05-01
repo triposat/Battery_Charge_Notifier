494ea5f5e4d72a8d85fa1d9b00a2dd0a329da22e
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-01 16:30:41 +0530

    Update Battery_Charging.py

--- Battery_Charging.py
+++ Battery_Charging.py
@@ -1,14 +1,14 @@
 from plyer import notification
 import psutil
 import time
-seconds=300
+seconds = 300
 while True:
-    battery=psutil.sensors_battery()
-    percent=battery.percent
-    if percent>=90:
-        pluged=battery.power_plugged
-        plug="Plugged In" if pluged else "Plugged Not In"
-        if plug=="Plugged In":
+    battery = psutil.sensors_battery()
+    percent = battery.percent
+    if percent >= 90:
+        pluged = battery.power_plugged
+        plug = "Plugged In" if pluged else "Plugged Not In"
+        if plug == "Plugged In":
             notification.notify(title=f"Battery Percentage Reaches to {percent}%",
                                 message="Please Remove The Plug!!",
                                 app_icon="C:/Users/Dell/Downloads/bat.ico",
